d027c6ced0d9727bbb7dacfa7c00ddcbeb982199
Caerostris <keno@kenoschwalb.com>
2015-12-10 22:16:24 +0000

    add license

--- html/js/zerotier.js
+++ html/js/zerotier.js
@@ -1,3 +1,11 @@
+/* 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ * Copyright (c) 2015 Keno Schwalb
+ */
+
 var ZeroTier = function(base_url, auth_secret, callback) {
 	var self = this;
 	self.base_url = base_url;
